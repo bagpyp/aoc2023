9e5a3e1083dd90f5208aec1c4a53f4a6e651d14e
Robert Cunningham <rtc@bagpyp.net>
2024-04-23 08:21:33 -0700

    add/run black, ignore .idea

--- 1-trebuchet/solution.py
+++ 1-trebuchet/solution.py
@@ -1,14 +1,13 @@
-
 def solution(input: str):
-    
+
     # YOUR CODE GOES HERE
 
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    with open('1-trebuchet/input') as f:
+    with open("1-trebuchet/input") as f:
         input = f.read()
 
     print(solution(input))
